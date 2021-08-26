4a1ce0f9c298d5218a75f274814cd33d0d322060
Rekha Yallatti <ryallatti97@gmail.com>
2021-08-26 22:29:49 +0530

    understanding of static files from wwwroot folder and other than wwwroot folder

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,36 +30,24 @@ namespace BookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.Use(async(context, next) =>
-            //{
-            //    await context.Response.WriteAsync("hello its my first middleware");
-              
-            //     //Calling next middleware
-            //    // await next();
-            //});
-            
+            app.UseStaticFiles(); // middleware to use static files
+
+            //this is for when we use static files from other than wwwroot folder
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "DummyStaticFiles")),
+                RequestPath = "/DummyStaticFiles"
+            }); ;
+
             //Enabling Routing 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                // "/" indicates root folder
-                //endpoints.Map("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
-
-               // this routing  points default to the Home controller
+              
                 endpoints.MapDefaultControllerRoute();
             });
-            //Custom middleware
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.Map("/Rekha", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello Rekha!");
-            //    });
-            //});
+           
         }
     }
 }
