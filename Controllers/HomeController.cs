d90e031f60028a1821f0b8dc7b21c3aae75e0c7d
Rekha Yallatti <ryallatti97@gmail.com>
2021-08-28 15:21:15 +0530

    viewDataAttribute added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,14 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        
+        [ViewData]
+        public string Title { get; set; }
+
+        [ViewData]
+        public BookModel book { get; set; }
         public ViewResult Index()
         {
             //dynamic data = new ExpandoObject();
@@ -21,6 +29,13 @@ namespace BookStore.Controllers
         }
         public ViewResult Aboutus()
         {
+            //viewdata attribute 
+            CustomProperty = "Custom Value";
+
+            Title = "Home Page";
+
+            book = new BookModel() { Id = 3, Title = "Asp.Net core tutorial" };
+            //viewdata
             ViewData["Property1"] = "Nitish";
             ViewData["Book"] = new BookModel() { Id = 1, Author = "Rekha" };
             return View();
