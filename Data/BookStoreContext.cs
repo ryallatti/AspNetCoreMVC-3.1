36239764300ea0669fd90457e1c71031101de97f
Rekha Yallatti <ryallatti97@gmail.com>
2021-08-29 23:23:31 +0530

    Created DBContext class, entity Class and added connection string

---
+++ Data/BookStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Books> Books { get; set; }
+
+        //if we add connection string in startup file then no need to add here
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=BookStore;Integrated Security=True;");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using BookStore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -18,10 +20,11 @@ namespace BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //adding MVC  to application 
+            services.AddDbContext<BookStoreContext>(options => options.UseSqlServer("Server=.\\SQLEXPRESS;Database=BookStore;Integrated Security=True;"));
+
+
             services.AddControllersWithViews();
-            // this is for rutime compilation for razor pages and also installed runtime compilation Package from nuget manager
-            //this increase the performance issue so we need to add this conditionally using preprocesser directory
+        
 #if DEBUG
             services.AddRazorPages().AddRazorRuntimeCompilation();
 #endif
